24387d3f73d3b10adc2e467c5707f17369d9197e
Jshewmaker <jshewmak@gmail.com>
2017-11-14 14:55:04 -0500

    Initial Commit

---
+++ Loon_Talking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Loon_Talking
+//
+//  Created by Joshua Shewmaker on 11/14/17.
+//  Copyright © 2017 Joshua Shewmaker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
